ad58ad919c9585eb6fdbeff91eb1d4ec5d53f0e2
ravibp <ravi.prasanna@infosys.com>
2019-09-07 02:46:41 +0530

    clipboard-app_v1.0
    connection to db complete

--- src/App.js
+++ src/App.js
@@ -1,17 +1,11 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Clipboard from './Clipboard.js';
+import Clipboard from './Clipboard';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <Clipboard />
-      </header>
+      <Clipboard />
     </div>
   );
 }
--- src/Clipboard.js
+++ src/Clipboard.js
@@ -1,31 +1,83 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import Rebase from "re-base";
+import * as firebase from "firebase/app";
+import "firebase/database";
+import { DB_CONFIG } from "./Config/config";
 
 class Clipboard extends React.Component {
   constructor(props) {
     super(props);
+    this.app = firebase.initializeApp(DB_CONFIG);
+
+    this.db_texts = this.app
+      .database()
+      .ref()
+      .child("texts"); // refers to db.collection.texts field
+  
     this.state = {
-        textValue: null
-    }
+      inputText: "",
+      texts: []
+    };
   }
-  handleInputChange = (event) => {
+  handleInputChange = event => {
     this.setState({
-        textValue : event.target.value 
-    })
-  }
+      [event.target.name]: event.target.value
+    });
+  };
   handleSubmit = () => {
-      console.log("submitted")
+    let textObj = {
+      id: this.state.texts.length + 1,
+      textValue: this.state.inputText
+    };
+    this.setState({
+      inputText: ""
+    });
+    // put data to db
+    this.db_texts.push().set(textObj);
+  };
+  componentWillMount() {
+    const prevTexts = this.state.texts;
+
+    // Data Snapshot, load db data into local state on page load and on each submit
+
+    this.db_texts.on("child_added", snap => {
+      let textObj = {
+        id: this.state.texts.length + 1,
+        textValue: snap.val().textValue // textValue from DB texts array.
+      };
+      prevTexts.push(textObj);
       this.setState({
-        textValue : ""
-    })
+        texts: prevTexts
+      });
+    });
   }
+  componentWillUnmount() {}
   render() {
     return (
       <div>
-        <p>Hello child</p>
-        <textArea>aaa</textArea>
-        <input name="clipboardInput" value={this.state.textValue} onChange={this.handleInputChange} type="text" />
-        <button type="button" onClick={this.handleSubmit}>Submit</button>
+        <p>My ClipBoard</p>
+        <input
+          name="inputText"
+          value={this.state.inputText}
+          onChange={this.handleInputChange}
+          type="text"
+        />
+        <div>
+          <ul>
+            {this.state.texts.length > 0 &&
+              this.state.texts.map((text, index) => {
+                return (
+                  <li key={index}>
+                    {text.id} {text.textValue}
+                  </li>
+                );
+              })}
+          </ul>
+        </div>
+        <button type="button" onClick={this.handleSubmit}>
+          Submit
+        </button>
       </div>
     );
   }
