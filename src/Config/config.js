bd45e7138d0c2e39218710b68bd36385a128e3e5
ravibp <ravi.prasanna@infosys.com>
2019-09-07 20:33:12 +0530

    remove favicon

---
+++ src/Config/config.js
@@ -0,0 +1,9 @@
+export const DB_CONFIG = {
+    apiKey: "AIzaSyAmtvUaZF1ZCga4zaJwtYKwi23shWphxEM",
+    authDomain: "clipboard-application.firebaseapp.com",
+    databaseURL: "https://clipboard-application.firebaseio.com",
+    projectId: "clipboard-application",
+    storageBucket: "",
+    messagingSenderId: "819825292187",
+    appId: "1:819825292187:web:8ddaf5a31a0a031bade73e"
+  };
