8d12f768b4857892a19fc5213a32e111dc1a866c
ravibp <ravi.prasanna@searshc.com>
2019-09-24 19:28:52 +0530

    enviroment setup

--- src/config/Config.js
+++ src/config/Config.js
@@ -1,16 +1,13 @@
 import * as firebase from 'firebase';
-
 const DB_CONFIG = {
-    apiKey: "AIzaSyAmtvUaZF1ZCga4zaJwtYKwi23shWphxEM",
-    authDomain: "clipboard-application.firebaseapp.com",
-    databaseURL: "https://clipboard-application.firebaseio.com",
-    projectId: "clipboard-application",
-    storageBucket: "",
-    messagingSenderId: "819825292187",
-    appId: "1:819825292187:web:8ddaf5a31a0a031bade73e"
-  };
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DB_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MSG_SENDER_ID,
+  appId: process.env.REACT_APP_ID,
+};
 
-  firebase.initializeApp(DB_CONFIG);
-  export const databaseRef = firebase.database().ref();
-  // export const textsRef = databaseRef.child("texts")
-  // export const usersRef = databaseRef.child("users")
+firebase.initializeApp(DB_CONFIG);
+export const databaseRef = firebase.database().ref();
