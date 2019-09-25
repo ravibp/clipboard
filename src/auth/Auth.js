f65908424361658db6f113698c9651832d5a0b56
ravibp <ravi.prasanna@infosys.com>
2019-09-26 00:58:05 +0530

    env changes

--- src/auth/Auth.js
+++ src/auth/Auth.js
@@ -40,8 +40,8 @@ export function renderAuthUI(ui) {
       // Leave the lines as is for the providers you want to offer your users.
       firebase.auth.GoogleAuthProvider.PROVIDER_ID,
       firebase.auth.FacebookAuthProvider.PROVIDER_ID,
-      firebase.auth.TwitterAuthProvider.PROVIDER_ID,
-      firebase.auth.GithubAuthProvider.PROVIDER_ID,
+      // firebase.auth.TwitterAuthProvider.PROVIDER_ID,
+      // firebase.auth.GithubAuthProvider.PROVIDER_ID,
       firebase.auth.EmailAuthProvider.PROVIDER_ID,
       firebase.auth.PhoneAuthProvider.PROVIDER_ID
     ],
