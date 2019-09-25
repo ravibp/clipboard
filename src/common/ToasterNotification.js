90100ae88fde71f4ecf5a289baa8803f86adfa58
ravibp <ravi.prasanna@infosys.com>
2019-09-26 02:19:20 +0530

    code refactor

--- src/common/ToasterNotification.js
+++ src/common/ToasterNotification.js
@@ -2,7 +2,7 @@ import React from "react";
 import toaster from "toasted-notes";
 import "./ToasterNotification.scss";
 
-function showPopupNotification(message, notificationStylesClass) {
+function showPopupNotification(message, notificationStylesClass, textId, crudAction) {
   const notificationDiv = (
     <div className={notificationStylesClass + " notification-popup"}>
       {message}
@@ -11,6 +11,20 @@ function showPopupNotification(message, notificationStylesClass) {
   toaster.notify(notificationDiv, {
     duration: 2000
   });
+  if(textId) {
+    let animation = "sucessAnimation 2s";
+    if(crudAction === "delete") {
+      animation = "deleteAnimation 2s";
+    }
+    document.getElementById(
+      "text-" + textId
+    ).parentElement.style.animation = animation;
+    setTimeout(() => {
+      document.getElementById(
+        "text-" + textId
+      ).parentElement.style.animation = "";
+    }, 1000);
+  }
 }
 
 export default showPopupNotification;
