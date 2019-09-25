c2cf894692d589dd9e7bab30c8d26d38844a37d0
ravibp <ravi.prasanna@infosys.com>
2019-09-26 02:19:20 +0530

    code refactor

--- src/ModalPopup.js
+++ src/ModalPopup.js
@@ -30,17 +30,25 @@ class ModalPopup extends Component {
     switch (crudOperation) {
       case "DELETE":
         let textId = this.props.textObj ? this.props.textObj.id : null;
-        this.props.deleteTextDB(textId, this.props.user);
-        showPopupNotification("Successfully Deleted!!! ", "notify-delete");
+        showPopupNotification(
+          "Successfully Deleted!!! ",
+          "notify-delete",
+          textId,
+          "delete"
+        );
+        setTimeout(() => {
+          this.props.deleteTextDB(textId, this.props.user);
+        }, 1000);
         this.props.modalToggle();
         break;
       case "UPDATE":
         this.props.updateTextDB(this.props.updatedTextObj, this.props.user);
         this.props.modalToggle();
-        document.getElementById(
-          "text-" + this.props.updatedTextObj.id
-        ).style.animation = "sucessAnimation 2s";
-        showPopupNotification("Changes Saved!!! ", "notify-update");
+        showPopupNotification(
+          "Changes Saved!!! ",
+          "notify-update",
+          this.props.updatedTextObj.id
+        );
         break;
       default:
         this.props.modalToggle();
