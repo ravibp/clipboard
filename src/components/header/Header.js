fa6903663526d4dcc7c7a3b5ea3f3532cf2cfaa1
ravibp <ravi.prasanna@infosys.com>
2020-01-30 21:06:58 +0530

    search responsive functionality complete

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,25 +1,15 @@
 import React from "react";
 import "./Header.scss";
-import Hamburger from "./Hamburger";
+// import HamburgerMenu from "react-hamburger-menu";
 import * as firebase from "firebase";
 const isMobileOnly = window.innerWidth <= 767 ? true : false;
 
 class Header extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
   render() {
-    const { user, displayName } = this.props;
-    const SignInOutButton = ({ uid, displayName }) => {
-      return (
-        <button
-          onClick={() => {
-            uid === "@Guest"
-              ? window.location.replace("/") // redirect to dashboard route "/"
-              : firebase.auth().signOut();
-          }}
-        >
-          {uid === "@Guest" ? "Login/ Signup" : `Logout, ${displayName}`}
-        </button>
-      );
-    };
     return (
       <div className="row no-gutters">
         <div className="col-9 col-md-6 header-title">
@@ -27,27 +17,30 @@ class Header extends React.Component {
             <li>My Clipboard</li>
           </ul>
         </div>
-        {isMobileOnly && (
-          <div className="hamburgerMenu">
-            <Hamburger
-              user={this.props.user}
-              displayName={this.props.displayName}
-              SignInOutButton={SignInOutButton}
-            />
-          </div>
-        )}
-        {!isMobileOnly && (
-          <div className="col-3 col-md-6 header-userInfo">
-            <ul>
-              <li>{`Welcome ${displayName}`}</li>
-              {user && user.uid && (
-                <li>
-                  <SignInOutButton uid={user.uid} displayName={displayName} />
-                </li>
-              )}
-            </ul>
-          </div>
-        )}
+        <div className="col-3 col-md-6 header-userInfo">
+          <ul>
+            <li>
+              {!isMobileOnly && `Welcome `} {this.props.displayName}
+            </li>
+            {this.props.user && this.props.user.uid && (
+              <li>
+                <button
+                  onClick={() => {
+                    let uid = this.props.user.uid;
+                    uid === "@Guest" && window.location.replace("/");
+                    uid !== "@Guest" && firebase.auth().signOut();
+                  }}
+                >
+                  {this.props.user.uid === "@Guest"
+                    ? !isMobileOnly
+                      ? "Login/ Signup"
+                      : "Login"
+                    : "Logout"}
+                </button>
+              </li>
+            )}
+          </ul>
+        </div>
       </div>
     );
   }
